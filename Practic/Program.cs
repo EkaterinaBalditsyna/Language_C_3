f6e24aa4682e78c3e99f3b5c8009bc1f2596ee5a
EBalditsyn <kat.balditsyna@yandex.ru>
2023-04-27 10:49:17 +0300

    Практическая 3

---
+++ Practic/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y)
+
+void ShowRange(int quad)
+{
+    if(quad == 1) Console.WriteLine("x > 0 and y > 0");
+    else if(quad == 2) Console.WriteLine("x < 0 and y > 0")
+    else if(quad == 3) Console.WriteLine("x < 0 and y > 0");
+    else if(quad == 4) Console.WriteLine("x > 0 and y < 0");
+    else Console.WriteLine("Wrong number of quadrant");
+}
+
+Console.WriteLine("Input a number of quadrant:");
+
+ShowRange(quadrant);
+
+//Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу квадратов чисел от 1 до N.
+
+//Напишите программу, которая принимает 
+//на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+//int x = (input ("Введите число:"));
+//int y = (input ("Введите число:"));
+
+int X, int Y
+
+return)
+{
+    if (x > 0 and y > 0)Console.WriteLine(num == 1);
+    else if(x < 0 and y > 0)Console.WriteLine(num == 2); 
+    else if (x < 0 and y > 0)Console.WriteLine(num == 3);
+    else if (x > 0 and y < 0)Console.WriteLine (num == 4);
+    else Console.WriteLine("Wrong number of quadrant");
+}
+
+Console.WriteLine("Input a X coordinate:");
+Console.WriteLine("Input a Y coordinate:");
